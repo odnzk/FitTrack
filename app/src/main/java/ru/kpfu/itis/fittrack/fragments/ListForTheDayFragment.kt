8e9977211a7d4fdbddac0f0cb2e71fee791bc24e
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-04 15:37:18 +0300

    fragments init

---
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/ListForTheDayFragment.kt
@@ -0,0 +1,12 @@
+package ru.kpfu.itis.fittrack.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.kpfu.itis.fittrack.R
+
+class ListForTheDayFragment : Fragment(R.layout.fragment_list_for_the_day) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
