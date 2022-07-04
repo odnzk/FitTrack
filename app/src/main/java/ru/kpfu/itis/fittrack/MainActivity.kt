55162065e3805c1eab40f7c9ed066553f0dee8fe
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-04 18:51:11 +0300

    change logic in activity_main.xml and add FloatingActionButton

--- app/src/main/java/ru/kpfu/itis/fittrack/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.kpfu.itis.fittrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        // fix background
+        bottomNavigationView.background = null
+        // Placeholder
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        controller = (supportFragmentManager.findFragmentById(R.id.container)
+                as NavHostFragment).navController
+        bottomNavigationView.setupWithNavController(controller)
     }
 }
