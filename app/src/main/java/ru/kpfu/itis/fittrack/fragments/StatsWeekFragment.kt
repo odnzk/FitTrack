2cf9224370d74861f1c0d341ba318daad5b37a9d
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-06 19:45:06 +0300

    Bar chart init

--- app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
@@ -3,10 +3,34 @@ package ru.kpfu.itis.fittrack.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import ru.kpfu.itis.fittrack.R
+import ru.kpfu.itis.fittrack.databinding.FragmentStatsWeekBinding
 
 class StatsWeekFragment : Fragment(R.layout.fragment_stats_week) {
+
+    private var _binding: FragmentStatsWeekBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentStatsWeekBinding.bind(view)
+
+        with(binding) {
+            barGraph.data = BarData(
+                BarDataSet(
+                    arrayListOf(BarEntry(44f, 0f), BarEntry(44f, 1f)),
+                    "Dates",
+                )
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
