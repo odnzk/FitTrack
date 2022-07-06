e37e2f7ee770c2859120d1a9e9d9a9444386920b
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-06 04:40:54 +0300

    change logic Floating action button

--- app/src/main/java/ru/kpfu/itis/fittrack/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/MainActivity.kt
@@ -4,12 +4,15 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import ru.kpfu.itis.fittrack.databinding.ActivityMainBinding
+import ru.kpfu.itis.fittrack.fragments.ProductsAndRecipesFragment
 import ru.kpfu.itis.fittrack.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +32,19 @@ class MainActivity : AppCompatActivity() {
             bottomNavigationView.setupWithNavController(controller)
 
             fab.setOnClickListener {
-                controller.navigate(R.id.productsAndRecipesFragment)
+                controller.navigate(
+                    R.id.productsAndRecipesFragment
+                )
+
+                hideMenu()
             }
+        }
+    }
+
+    private fun hideMenu() {
+        with (binding) {
+            fab.hide()
+            bottomAppBar.performHide()
         }
     }
 
--- app/src/main/java/ru/kpfu/itis/fittrack/fragments/ProductsAndRecipesFragment.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/ProductsAndRecipesFragment.kt
@@ -1,12 +1,26 @@
 package ru.kpfu.itis.fittrack.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.kpfu.itis.fittrack.R
 
 class ProductsAndRecipesFragment : Fragment(R.layout.fragment_products_and_recipes) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        showMenu()
+        super.onDestroyView()
+    }
+
+    private fun showMenu() {
+        activity?.findViewById<FloatingActionButton>(R.id.fab)?.show()
+        activity?.findViewById<BottomAppBar>(R.id.bottomAppBar)?.performShow()
     }
 }
