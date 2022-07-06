486a40da7e57b9116a6d5ae7310c8c23944ee152
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-06 22:04:46 +0300

    add methods in fragment

--- app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
@@ -15,18 +15,35 @@ class StatsWeekFragment : Fragment(R.layout.fragment_stats_week) {
     private var _binding: FragmentStatsWeekBinding? = null
     private val binding get() = _binding!!
 
+    var graphLabel: String = ""
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentStatsWeekBinding.bind(view)
 
+        graphLabel = "Calories"
+
+        setGraphData(arrayListOf(1f, 2f, 3f, 4f, 5f, 6f, 7f), graphLabel)
+
+    }
+
+    private fun setGraphData(list: List<Float>, label: String) {
         with(binding) {
             barGraph.data = BarData(
-                BarDataSet(
-                    arrayListOf(BarEntry(44f, 0f), BarEntry(44f, 1f)),
-                    "Dates",
-                )
+                createDataSetFromList(list, label)
             )
         }
+    }
+
+    private fun createDataSetFromList(list: List<Float>, label: String): BarDataSet {
+        val entryList = ArrayList<BarEntry>()
+        var i = 0f
+        list.forEach {
+            entryList.add(
+                BarEntry(i++, it)
+            )
+        }
+        return BarDataSet(entryList, label)
     }
 
     override fun onDestroyView() {
