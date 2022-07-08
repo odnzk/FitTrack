b9dfed89798f5b18d7480667903c91c8609cb117
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-08 07:12:17 +0300

    optimizing imports

--- app/src/main/java/ru/kpfu/itis/fittrack/fragments/ProductsAndRecipesFragment.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/ProductsAndRecipesFragment.kt
@@ -1,12 +1,8 @@
 package ru.kpfu.itis.fittrack.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.kpfu.itis.fittrack.R
 
 class ProductsAndRecipesFragment : Fragment(R.layout.fragment_products_and_recipes) {
