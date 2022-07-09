bca68480e4c974d7f6d6fecb3f46f6f0be24e537
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-07-09 06:36:25 +0300

    adding items

--- app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/fragments/StatsWeekFragment.kt
@@ -1,12 +1,10 @@
 package ru.kpfu.itis.fittrack.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import ru.kpfu.itis.fittrack.R
 import ru.kpfu.itis.fittrack.databinding.FragmentStatsWeekBinding
 import ru.kpfu.itis.fittrack.util.BarChartProcessor
@@ -28,32 +26,33 @@ class StatsWeekFragment : Fragment(R.layout.fragment_stats_week) {
             description.text = ""
         }
 
-        val graphLabel = "Calories"
+        init()
+
+    }
+
+    private fun init() {
         val data = arrayListOf(
-            1f,
-            2f,
-            3f,
-            4f,
-            5f,
-            6f,
-            7f
+            0f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f
         )
         val strings = arrayListOf(
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         )
-
-        with(processor) {
-            setGraphDataFromList(data, graphLabel)
+        processor.apply {
+            setGraphDataFromList(data)
             setStringFields(strings)
         }
-
-
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ru/kpfu/itis/fittrack/util/BarChartProcessor.kt
+++ app/src/main/java/ru/kpfu/itis/fittrack/util/BarChartProcessor.kt
@@ -1,6 +1,7 @@
 package ru.kpfu.itis.fittrack.util
 
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
@@ -8,10 +9,18 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
 class BarChartProcessor(barChart: BarChart) {
     private val chart = barChart
+    private val label = "Calories"
 
-    fun setGraphDataFromList(list: List<Float>, label: String) {
+    init {
+        chart.getAxis(YAxis.AxisDependency.LEFT).axisMinimum = 0f
+        chart.xAxis.setDrawGridLines(false)
+        chart.axisLeft.setDrawGridLines(false)
+        chart.axisRight.setDrawGridLines(false)
+    }
+
+    fun setGraphDataFromList(list: List<Float>) {
         chart.data = BarData(
-            createDataSetFromList(list, label)
+            createDataSetFromList(list)
         )
     }
 
@@ -19,7 +28,29 @@ class BarChartProcessor(barChart: BarChart) {
         chart.xAxis.valueFormatter = IndexAxisValueFormatter(list)
     }
 
-    private fun createDataSetFromList(list: List<Float>, label: String): BarDataSet {
+    fun addData(item: Float, info: String) {
+        val listData = ArrayList<Float>()
+        val listInfo = ArrayList<String>()
+        val dataSet = chart.data.dataSets[0]
+        dataSet.apply {
+            for (i in 1 until entryCount) {
+                val entry = getEntryForIndex(i)
+                listData.add(entry.y)
+                listInfo.add(
+                    chart
+                        .xAxis
+                        .valueFormatter
+                        .getFormattedValue(entry.x)
+                )
+            }
+        }
+        listData.add(item)
+        listInfo.add(info)
+        setGraphDataFromList(listData)
+        setStringFields(listInfo)
+    }
+
+    private fun createDataSetFromList(list: List<Float>): BarDataSet {
         val entryList = ArrayList<BarEntry>()
         var i = 0f
         list.forEach {
